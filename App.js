cd9653e89bc9073f1ecac544edd67763d247f9e3
Jack Murphy <signal.space@protonmail.com>
2020-12-18 13:13:16 -0600

    basic example

--- App.js
+++ App.js
@@ -1,16 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import * as React from 'react';
+// import { StyleSheet, Text, View, Button } from 'react-native';
+// import { Audio } from 'expo-av';
+// // import {Button} from "react-native-web";
+//
+// export default function App() {
+//   const [sound, setSound] = React.useState();
+//
+//   async function playSound() {
+//     console.log('Loading Sound');
+//     const { sound } = await Audio.Sound.createAsync(
+//         require('./assests/ambient_01.mp3')
+//     );
+//     setSound(sound);
+//
+//     console.log('Playing Sound');
+//     await sound.playAsync();
+//   }
+//
+//   React.useEffect(() => {
+//     return sound
+//     ? () => {
+//       console.log('Unloading Sound');
+//       sound.unloadAsync(); }
+//       : undefined;
+//   }, [sound]);
+//   return (
+//     <View style={styles.container}>
+//       <Button title="Play Sound" onPress{playSound} />
+//       {/*<StatusBar style="auto" />*/}
+//     </View>
+//   );
+// }
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+        require('./assets/ambient_01.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+        ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+        : undefined;
+  }, [sound]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Button title="Play Sound" onPress={playSound} />
+      </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Expo Audio Test App
+This is a simple first step in learning how to build ios app with audio using reactive native  and expo.
